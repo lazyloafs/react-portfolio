ffdd0a5f0b6b5ead97860c29c12e9ff9c66054c8
unknown <lazyloafs@gmail.com>
2020-08-20 18:21:20 -0400

    trying another time

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import About from "./pages/About";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-// import { Redirect } from 'react-router-dom'
 import { Container } from  'react-bootstrap'
 import NavbarCustom from "./components/Navbar";
 import Footer from "./components/Footer";
 import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
-// import { Switch } from 'react'
 
 
 
@@ -19,6 +17,7 @@ function App() {
       <div>
         <NavbarCustom />
         <Container>
+          <Route exact path="/" component={Portfolio} /> 
           <Route exact path="/Portfolio" component={Portfolio} />
           <Route exact path="/About" component={About} />
           <Route exact path="/Contact" component={Contact} />
